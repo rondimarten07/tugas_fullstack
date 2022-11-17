1e488feed32a6db4a3175f81b81f0fca9ba2df53
budi <susucoklat341@gmail.com>
2022-11-17 14:28:53 +0700

    Update tentang.php

--- application/views/depan/tentang.php
+++ application/views/depan/tentang.php
@@ -1,15 +1,17 @@
-<div class="container">    
+<div class="container">
   <div class="row">
     <div class="col-sm-8">
       <div class="panel panel-default">
         <div class="panel-body">
-		
-		<?php echo $web->tentang ?>
-		
-		</div>
-	  </div>
+
+          <?php echo $web->tentang ?>
+
+        </div>
+      </div>
+    </div>
+    <div>
+      halo budi
     </div>
 
   </div>
 </div>
-

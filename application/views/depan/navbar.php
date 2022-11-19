99c887b5d79117a83aa9fc84b2b8d60360105e8c
rondimarten07 <rondimarten@gmail.com>
2022-11-19 09:00:46 +0700

    mengubah tampilan home

--- application/views/depan/navbar.php
+++ application/views/depan/navbar.php
@@ -1,38 +1,90 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" style="color: #E5B299" href="<?php echo site_url() ?>"><i
+                    class="fa fa-home"></i></a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="<?php echo ($page == 'home') ? 'active' : '' ?>"><a href="<?php echo site_url() ?>"
+                        style="color: #E5B299">Home</a>
+                </li>
+
+                <!-- Resep -->
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                        aria-expanded="false" style="color: #E5B299">Resep <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li class="<?php echo ($page == 'produk') ? 'active' : '' ?>"><a
+                                href="<?php echo site_url('produk') ?>">Makanan Berkuah</a></li>
+                        <li><a href="#">Minuman</a></li>
+                        <li><a href="#">Cemilan</a></li>
+                        <li><a href="#">Makanan Goreng</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+
+                <li><a href="<?php echo site_url('admin') ?>" style="color: #E5B299">Masuk <i
+                            class="fa fa-sign-in"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <div class="jumbotron">
-  <div class="container">
-	<div class="col-md-6 judul">
-		<a href="<?php echo site_url() ?>">
-		<img src="<?php echo base_url('assets/tjc/img/logo-tangsi.png') ?>" class="pull-left" style="display:inline; max-width:90px; background: rgba(0,0,0,0.3); border-radius:50%;margin-right:32px"/>
-		<h3><?php echo $web->judul ?></h3>
-		<p style="font-size:12px"><?php echo $web->tagline ?></p>
-		</a>
-	</div>
-  </div>
+    <div class="container">
+        <div class="col-md-4">
+        </div>
+
+        <div class="col-md-4 text-center">
+            <img src="<?php echo base_url('assets/tjc/img/logo-tangsi1.png') ?>"
+                style="display:inline-block max-width:20px; background: rgba(0,0,0,0.3); border-radius:50%;" />
+            <a href="<?php echo site_url() ?>">
+
+                <h2 style="color: white;">
+                    <?php echo $web->judul ?>
+                </h2>
+                <p style=" font-size:12px; color: white">
+                    <?php echo $web->tagline ?>
+                </p>
+            </a>
+
+        </div>
+        <div class="col-md-4  ">
+        </div>
+
+    </div>
 </div>
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="<?php echo site_url() ?>"><i class="fa fa-home"></i></a>
+
+</div>
+<div class="container">
+    <div class="col-md-4">
     </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="<?php echo ($page == 'home') ?'active':'' ?>"><a href="<?php echo site_url() ?>">Home</a></li>
-        <li class="<?php echo ($page == 'produk') ?'active':'' ?>"><a href="<?php echo site_url('produk') ?>">Produk</a></li>
-        <li class="<?php echo ($page == 'jasa') ?'active':'' ?>"><a href="<?php echo site_url('jasa') ?>">Layanan &amp; Jasa</a></li>
-        <li class="<?php echo ($page == 'promo') ?'active':'' ?>"><a href="<?php echo site_url('promo') ?>">Promo</a></li>
-        <li class="<?php echo ($page == 'paket') ?'active':'' ?>"><a href="<?php echo site_url('paket') ?>">Paket</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <li class="<?php echo ($page == 'tentang') ?'active':'' ?>"><a href="<?php echo site_url('tentang') ?>">Tentang Kami</a></li>
-        <li class="<?php echo ($page == 'kontak') ?'active':'' ?>"><a href="<?php echo site_url('kontak') ?>">Kontak</a></li>
-        <li><a href="<?php echo site_url('admin') ?>">Masuk <i class="fa fa-sign-in"></i></a></li>
-      </ul>
+
+    <div class="col-md-4 text-center formcari">
+        <form class="navbar-form">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Cari!</button>
+        </form>
+
     </div>
-  </div>
-</nav>
+    <div class="col-md-4  ">
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="col-md-12 text-center">
+        <h1 class="judulkonten">KUMPULAN MASAKAN BISA RESEP</h1>
+        <h6 class="judulkonten2">dijamin berkualitas</h6>
+    </div>
+</div>
--- application/views/depan/tentang.php
+++ application/views/depan/tentang.php
@@ -1,15 +1,14 @@
-<div class="container">    
-  <div class="row">
-    <div class="col-sm-8">
-      <div class="panel panel-default">
-        <div class="panel-body">
-		
-		<?php echo $web->tentang ?>
-		
-		</div>
-	  </div>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <?php echo $web->tentang ?>
+
+                </div>
+            </div>
+        </div>
+
     </div>
-
-  </div>
 </div>
-

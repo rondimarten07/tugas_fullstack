f54482ce23cf952a9d3577f4ec8cc212bb49afcf
rondimarten07 <rondimarten@gmail.com>
2022-11-25 00:38:50 +0700

    update tampilan

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,10 +1,11 @@
 	<footer>
-        <p class="text-center">&copy; <?php echo Date('Y') ?> <?php echo $web->judul ?></p>                  
-    </footer>
+	    <p class="text-center">&copy; <?php echo Date('Y') ?> <?php echo $web->judul ?></p>
+	</footer>
 
 
 
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js"></script>
+	</body>
+
+	</html>
